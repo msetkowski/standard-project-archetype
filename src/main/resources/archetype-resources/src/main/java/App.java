25333da22ee9801c6a41955b82d9c223e27e411d
setkomac <setkomac@WL2009556.eu.tieto.com>
2012-08-06 08:48:31 +0200

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $pl.wowbagger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
